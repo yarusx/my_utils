b7af23ec32e954f34bd27d5b591c95e2161144e0
Yaroslav Khoruzhenko <yarusx@gmail.com>
2020-11-27 18:02:49 +0200

    added small script for an automatic creation of the classes anotation with saving in class_names.txt

--- dir_from_path.py
+++ dir_from_path.py
@@ -3,6 +3,8 @@ import glob
 import shutil
 from os import path
 
+class_names = []
+
 parent_dir = "/Users/yarusx/Downloads/images/"
 try:
     os.mkdir('/Users/yarusx/Downloads/images/Dog')
@@ -29,6 +31,7 @@ for file in filename:
     temp_dirname = temp_dirname.split("_")[:ind-1]
     temp_dirname = '_'.join(temp_dirname)
 
+    if not str(temp_dirname) in class_names: class_names.append(str(temp_dirname))
 
     if str(os.path.splitext(file)[0])[31].isupper() is True:
         current_dir = os.path.join(sub_dir_cat, temp_dirname)
@@ -49,3 +52,7 @@ for file in filename:
             pass
 
         shutil.move(file,current_dir)
+
+file = open(parent_dir+"/class_names.txt", "w")
+file.write(str(class_names))
+file.close()
