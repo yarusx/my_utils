4671e4284b73c81f6ee4a7e4a61226a18a6a5bba
Yaroslav Khoruzhenko <yarusx@gmail.com>
2020-11-29 09:57:40 +0200

    splited class names into 2 files (dogs,cats)

---
+++ dirs_labels_from_path.py
@@ -0,0 +1,65 @@
+import os
+import glob
+import shutil
+from os import path
+
+dogs_names = []
+cats_names = []
+
+parent_dir = "/Users/yarusx/Downloads/images/"
+try:
+    os.mkdir('/Users/yarusx/Downloads/images/Dog/')
+except:
+    pass
+
+try:
+    os.mkdir('/Users/yarusx/Downloads/images/Cat/')
+except:
+    pass
+
+sub_dir_dog = '/Users/yarusx/Downloads/images/Dog/'
+sub_dir_cat = '/Users/yarusx/Downloads/images/Cat/'
+
+temp_dirname = ''
+current_dir = ''
+
+filename = glob.glob(parent_dir+"*.jpg")
+
+for file in filename:
+
+    temp_dirname = str(os.path.splitext(file)[0])[31:].lower()
+    ind = int(len(temp_dirname.split("_")))
+    temp_dirname = temp_dirname.split("_")[:ind-1]
+    temp_dirname = '_'.join(temp_dirname)
+
+    if str(os.path.splitext(file)[0])[31].isupper() is True:
+        current_dir = os.path.join(sub_dir_cat, temp_dirname)
+        #print(current_dir)
+        try:
+            os.mkdir(sub_dir_cat+"/"+temp_dirname)
+        except:
+            pass
+
+        shutil.move(file,current_dir)
+
+        if not str(temp_dirname) in cats_names: cats_names.append(str(temp_dirname))
+
+    if str(os.path.splitext(file)[0])[31].isupper() is False:
+        current_dir = os.path.join(sub_dir_dog, temp_dirname)
+        #print(current_dir)
+        try:
+            os.mkdir(sub_dir_dog+"/"+temp_dirname)
+        except:
+            pass
+
+        shutil.move(file,current_dir)
+
+        if not str(temp_dirname) in dogs_names: dogs_names.append(str(temp_dirname))
+
+file_d = open(sub_dir_dog+"dogs_names.txt", "w")
+file_d.write(str(dogs_names))
+file_d.close()
+
+file_d = open(sub_dir_cat+"cats_names.txt", "w")
+file_d.write(str(cats_names))
+file_d.close()
